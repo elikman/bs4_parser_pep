a3f05d25ea5e4ef1c20a0c0b4a9ac18f153100d9
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2024-10-31 16:21:27 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 from typing import Tuple, Optional, List
 from requests import Session, Response
-from exceptions import PythonVersionsNotFound, ParserFindTagException
 
 from constants import (
     BASE_DIR,
@@ -23,6 +22,7 @@ from configs import configure_argument_parser, configure_logging
 from collections import defaultdict
 from outputs import control_output
 from utils import get_response, find_tag
+from exceptions import PythonVersionsNotFound
 
 
 def get_response_and_soup(
@@ -68,15 +68,12 @@ def latest_versions(session: Session) -> List[Tuple[str, str, str]]:
     sidebar = find_tag(
         soup, HTMLTag.DIV, attrs={'class': 'sphinxsidebarwrapper'}
     )
-
     ul_tags = sidebar.find_all(HTMLTag.UL)
     for ul in ul_tags:
         if 'All versions' in ul.text:
             break
     else:
-        raise PythonVersionsNotFoundException(
-            'Не найден список c версиями Python'
-        )
+        raise PythonVersionsNotFound('Не найден список c версиями Python')
 
 
 def download(session: Session) -> None:
@@ -102,40 +99,31 @@ def download(session: Session) -> None:
 def pep(session):
     dict_results = defaultdict(int)
     results = [('Статус', 'Количество')]
-    
     response = get_response(session, MAIN_DOC_PEP_URL)
     if response is None:
         return
-        
     soup = BeautifulSoup(response.text, 'lxml')
     find_all_tags = soup.find_all('td')
-    
     for tag in tqdm(find_all_tags):
         text_match = re.search(PATTERN_NUMBER_OF_PEP, tag.text)
         if not text_match:
             continue
-            
         short_link = tag.find('a')['href']
         full_url = urljoin(MAIN_DOC_PEP_URL, short_link)
         status_key = tag.find_previous_sibling('td').text.strip()
-        
         pep_response = get_response(session, full_url)
         if pep_response is None:
             continue
-            
         pep_soup = BeautifulSoup(pep_response.text, 'lxml')
         find_tag_dt = find_tag(
             pep_soup, 'dt', attrs={'class': ['field-even', 'field-odd']}
         )
-        
         while find_tag_dt and find_tag_dt.text != 'Status:':
             find_tag_dt = find_tag_dt.find_next_sibling(
                 'dt', {'class': ['field-even', 'field-odd']}
             )
-            
         status_in_card = find_tag_dt.find_next_sibling('dd').text
         dict_results[status_in_card] += 1
-        
         expected_status = EXPECTED_STATUS[status_key[1:]]
         if status_in_card not in expected_status:
             logging.info(
@@ -144,7 +132,6 @@ def pep(session):
                 f'Статус в карточке: {status_in_card}\n'
                 f'Ожидаемые статусы: {expected_status}\n'
             )
-            
     results.extend(dict_results.items())
     results.append(('Total', sum(dict_results.values())))
     return results
@@ -159,26 +146,18 @@ MODE_TO_FUNCTION = {
 
 
 def main() -> None:
-
     configure_logging()
-
     logging.info('Парсер запущен!')
-
     arg_parser = configure_argument_parser(MODE_TO_FUNCTION.keys())
     args = arg_parser.parse_args()
-
     logging.info(f'Аргументы командной строки: {args}')
-
     session = requests_cache.CachedSession()
     if args.clear_cache:
         session.cache.clear()
-
     parser_mode = args.mode
     results = MODE_TO_FUNCTION[parser_mode](session)
-
     if results is not None:
         control_output(results, args)
-
     logging.info('Парсер завершил работу.')
 
 
